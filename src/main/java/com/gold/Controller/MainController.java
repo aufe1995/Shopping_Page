2152d156b761f5b2414209a3ec79c59e99d2cc74
aufe1995 <aufe1995@gmail.com>
2023-02-03 18:38:53 +0900

    separate controller

--- src/main/java/com/gold/Controller/MainController.java
+++ src/main/java/com/gold/Controller/MainController.java
@@ -1,62 +1,33 @@
 package com.gold.Controller;
 
-import com.gold.service.UserService;
-import com.gold.user.UserVo;
-import org.jetbrains.annotations.NotNull;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 public class MainController {
-	@Autowired
-	UserService userService;
+
+	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
 
 	//홈페이지
 	@GetMapping("/")
-	public String index() {
-		return "home";
+	public String mainPage() {
+
+		logger.info(">>>>>>>>>>>>>>>>>>> 홈페이지 진입");
+
+		return "main";
 	}
 
 	//구현 준비중인 페이지
-	@GetMapping("/otherpage")
-	public String otherPage() {
-		return "otherpage";
+	@GetMapping("/preparing")
+	public String preparingPage() {
+
+		logger.info(">>>>>>>>>>>>>>>>>>> 미완성 페이지 진입");
+
+		return "preparing";
 	}
 
-	//로그인 페이지
-	@GetMapping("/login")
-	public String login(@NotNull Model model, @RequestParam(value = "error", required = false) String error, @RequestParam(value = "exception", required = false) String exception) {
-		model.addAttribute("error", error);
-		model.addAttribute("exception", exception);
-		return "login";
-	}
 
-	@PostMapping("/login/action")
-	public String loginAction() {
-		return "redirect:/";
-	}
-
-	//회원가입 페이지
-	@GetMapping("/signup")
-	public String signUpForm() { return "signup"; }
-
-	//회원가입 실행 시
-	@PostMapping("/signup/action")
-	public String signUp(UserVo userVO) {
-		userService.joinUser(userVO);
-		return "redirect:/";
-	}
-
-	@ResponseBody
-	@PostMapping("/idCheck")
-	public int idCheck(String userID) throws Exception {
-		int result = userService.idCheck(userID);
-		return result;
-	}
 
 }
---
+++ src/main/java/com/gold/Controller/UserController.java
@@ -0,0 +1,94 @@
+package com.gold.Controller;
+
+import com.gold.service.UserService;
+import com.gold.user.UserVo;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class UserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @Autowired
+    UserService userService;
+
+    //관리자 페이지
+    @GetMapping("/admin")
+    public String adminPage() {
+
+        logger.info(">>>>>>>>>>>>>>>>>>> 관리자 페이지 진입");
+
+        return "admin";
+    }
+
+    //로그인 페이지
+    @GetMapping("/user/login")
+    public String loginPage(@NotNull Model model, @RequestParam(value = "error", required = false) String error, @RequestParam(value = "exception", required = false) String exception) {
+        model.addAttribute("error", error);
+        model.addAttribute("exception", exception);
+
+        logger.info(">>>>>>>>>>>>>>>>>>> 로그인 페이지 진입");
+
+        return "user/login";
+    }
+
+    //회원가입 페이지
+    @GetMapping("/user/join")
+    public String joinPage() {
+
+        logger.info(">>>>>>>>>>>>>>>>>>> 회원가입 페이지 진입");
+
+        return "user/join";
+    }
+
+    //회원가입 실행 시
+    @PostMapping("/join/action")
+    public String joinAction(UserVo userVO) {
+
+        userService.joinUser(userVO);
+
+        logger.info(">>>>>>>>>>>>>>>>>>> 회원가입 완료");
+
+        return "redirect:/user/login";
+    }
+
+
+    @ResponseBody
+    @PostMapping("/idCheck")
+    public int idCheck(String userID) throws Exception {
+        int result = userService.idCheck(userID);
+        return result;
+    }
+
+    @PostMapping("/login/action")
+    public String loginAction(HttpServletRequest request, HttpServletResponse response) {
+
+        logger.info(">>>>>>>>>>>>>>>>>>> 로그인 실행");
+        return "main";
+    }
+
+    @GetMapping("logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response){
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
+        logger.info(">>>>>>>>>>>>>>>>>>> 로그아웃 실행");
+        return "redirect:/";
+    }
+
+
+
+
+}
